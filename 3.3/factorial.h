c1865cfc900ea83773f2d62eb62d591e29a1ccad
datphamcode295 <vandatpro2000@gmail.com>
2021-03-21 17:47:12 +0700

    remove all

--- 3.3/factorial.h
+++
@@ -1,5 +0,0 @@
-// f a c t o r i a l . h
-#ifndef FACTORIAL_H
-#define FACTORIAL_H
-int factorial( const int aNumber ) ;
-#endif
--- 3.3/main.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-//#include "readline.h"
-#include "factorial.h"
-int main(int argc, char* argv[]) {
-// Implement m y f a c t o r i a l
-factorial(5);
-return 0;
-}
-
